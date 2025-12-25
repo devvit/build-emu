1605c38b62990b9901d03a368d9b121b6fe4d9e9
admin <admin@example.com>
2025-12-25 15:43:16 +0800

    Thu Dec 25 15:43:15 CST 2025  M .github/workflows/build.yml A  build.sh

---
+++ build.sh
@@ -0,0 +1,16 @@
+#
+
+git config --global url."https://github.com/".insteadOf "git@github.com:"
+git config --global url."https://github.com/".insteadOf "ssh://git@github.com/"
+
+git clone --depth 1 --recursive --shallow-submodules https://github.com/OpenEmu/OpenEmu
+
+cd OpenEmu
+build_dir=$(pwd)
+
+cd OpenEmu-SDK
+git checkout master
+git pull origin master
+
+cd $build_dir
+xcodebuild -workspace "OpenEmu.xcworkspace" -scheme "OpenEmu (Experimental)" -configuration Release -sdk macosx -arch x86_64 -derivedDataPath DerivedData
